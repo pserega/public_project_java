37e7676b2b0de03068c846f3c061209a937b4328
Sergey_Prig <Sergey_Prig>
2013-09-19 12:22:43 +0300

    add

---
+++ Test/src/main/java/by/prigozhiy/App.java
@@ -0,0 +1,21 @@
+package by.prigozhiy;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+	}
+
+	private String test(String[] s) {
+		// TODO не доработанное место
+		for (String string : s) {
+			System.out.println(string);
+		}
+		return null;
+
+	}
+}
