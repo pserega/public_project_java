72fae28082372fdb6a7f035af74b73ca4ea4b386
Sergey_Prig <Sergey_Prig@PC-IT-058.main.velcom.by>
2015-04-28 04:07:47 -0300

    test

--- Test/src/main/java/by/prigozhiy/App.java
+++ Test/src/main/java/by/prigozhiy/App.java
@@ -1,5 +1,13 @@
 package by.prigozhiy;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  * 
@@ -7,7 +15,40 @@ package by.prigozhiy;
 public class App {
 	public static void main(String[] args) {
 
-		System.out.println("Hello World .Hello Velcom ");
+		try {
+			PropertiesXMLWriteExample();
+			PropertiesXMLReadExample();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void PropertiesXMLWriteExample() throws IOException {
+		Properties props = new Properties();
+		props.setProperty("email.support", "p_serega_");
+
+		// where to store?
+		OutputStream os = new FileOutputStream(System.getProperty("user.dir") + File.separator + "email-configuration.xml");
+
+		// store the properties detail into a pre-defined XML file
+		props.storeToXML(os, "Support", "UTF-8");
+
+		System.out.println("Done");
+	}
+
+	private static void PropertiesXMLReadExample() throws IOException
+	{
+		Properties props = new Properties();
+		 
+    	InputStream is = new FileInputStream(System.getProperty("user.dir") + File.separator + "email-configuration.xml");
+    	//load the xml file into properties format
+    	props.loadFromXML(is);
+ 
+    	String email = props.getProperty("email.support");
+ 
+    	System.out.println(email);
 	}
 
 	private String test(String[] s) {
